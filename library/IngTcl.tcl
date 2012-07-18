d65686c19831bdddbb15851caee4504b0e546d33
Tomasz Konojacki <xenu@poczta.onet.pl>
2012-07-18 16:02:49 +0200

    added copyright notice to IngTcl.tcl

--- library/IngTcl.tcl
+++ library/IngTcl.tcl
@@ -1,3 +1,10 @@
+#
+# See the file "license.terms" for information on usage and redistribution
+# of this file, and for a DISCLAIMER OF ALL WARRANTIES.                   
+#                                                                       
+# Copyright (c) 2012 Tomasz Konojacki
+#
+
 package provide IngTcl 0.1;
 
 namespace eval ::IngTcl {
