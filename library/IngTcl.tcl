ac482ab90ddfe1208354fde1e850d4b3ccc1f1af
Tomasz Konojacki <me@xenu.tk>
2012-10-27 21:07:44 +0200

    added database detection procs

--- library/IngTcl.tcl
+++ library/IngTcl.tcl
@@ -15,3 +15,61 @@ proc ::IngTcl::quote {str} {
     regsub -all {'} $str {''} str
     return "'$str'"
 }
+
+proc ::IngTcl::dbms_name {session} {
+    set row {}
+    set cursor {}
+    set name {}
+
+    set cursor [IngTcl::prepare $session "SELECT dbmsinfo('_version')"]
+    IngTcl::execute $session $cursor
+    set row [IngTcl::fetch_row $session $cursor]
+
+    IngTcl::close_cursor $session $cursor
+    IngTcl::delete_cursor $session $cursor
+
+    if {[regexp {^\w{2}} [lindex $row 0] name]} {
+        switch $name {
+            II {
+                return Ingres
+            }
+
+            VW {
+                return Vectorwise
+            }
+
+            default {
+                return unknown
+            }
+        }
+    }
+    
+    return unknown
+}
+
+proc ::IngTcl::dbms_version {session} {
+    set row {}
+    set cursor {}
+    set version {}
+
+    set cursor [IngTcl::prepare $session "SELECT dbmsinfo('_version')"]
+    IngTcl::execute $session $cursor
+    set row [IngTcl::fetch_row $session $cursor]
+
+    IngTcl::close_cursor $session $cursor
+    IngTcl::delete_cursor $session $cursor
+
+    if {[regexp {^(II|VW) (\d+\.\d+\.\d+)} [lindex $row 0] {} {} version]} {
+        return $version
+    }
+    
+    return unknown
+}
+
+proc ::IngTcl::is_vectorwise {session} {
+    if {[::IngTcl::dbms_name $session] eq "Vectorwise"} {
+        return true
+    }
+
+    return false
+}
